f2e16fb44cca377b1d36b49ee862a915d09a4f61
Beste Tokpınar <bstkpnr01@gmail.com>
2024-05-08 12:47:19 +0300

    edit fix

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/Home/HomeScreen'
+import { useFonts } from 'expo-font'
+
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    "RobotoSlab-Black": require("./assets/fonts/RobotoSlab-Black.ttf"),
+    "RobotoSlab-Bold": require("./assets/fonts/RobotoSlab-Bold.ttf"),
+    "RobotoSlab-ExtraBold": require("./assets/fonts/RobotoSlab-ExtraBold.ttf"),
+    "RobotoSlab-ExtraLight": require("./assets/fonts/RobotoSlab-ExtraLight.ttf"),
+    "RobotoSlab-Light": require("./assets/fonts/RobotoSlab-Light.ttf"),
+    "RobotoSlab-Medium": require("./assets/fonts/RobotoSlab-Medium.ttf"),
+    "RobotoSlab-Regular": require("./assets/fonts/RobotoSlab-Regular.ttf"),
+    "RobotoSlab-SemiBold": require("./assets/fonts/RobotoSlab-SemiBold.ttf"),
+    "RobotoSlab-Thin": require("./assets/fonts/RobotoSlab-Thin.ttf"),
+    
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <HomeScreen />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const styles = StyleSheet.create({})
